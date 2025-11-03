a5c8db91353cbaceb6206942aacf3a42a2461cbc
Anush <aanush748@gmail.com>
2025-11-03 07:33:22 +0530

    Create Baka.py

---
+++ Baka.py
@@ -0,0 +1,26 @@
+from flask import Flask, send_from_directory
+import os
+import os 
+
+BASE = os.path.dirname(os.path.abspath(__file__))
+HTML_DIR = os.path.join(BASE, "html")
+CSS_DIR = os.path.join(BASE, "css")
+JS_DIR = os.path.join(BASE, "js")
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return send_from_directory(HTML_DIR,"todo.html")  # serves html/1.html
+
+@app.route("/css/<path:filename>")
+def serve_css(filename):
+    return send_from_directory(CSS_DIR, filename)   # serves css/*
+
+@app.route("/js/<path:filename>")
+def serve_js(filename):
+    return send_from_directory(JS_DIR, filename)    # serves js/*
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
