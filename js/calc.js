80625ac4ed870548101d30b59751c89c248972c2
akashhogetapal <akash85486@gmail.com>
2025-11-21 22:48:59 +0530

    dooo

--- js/calc.js
+++ js/calc.js
@@ -1,10 +1,18 @@
 window.onload=() => {
     
 }
+function isAlpha(ch) {
+    return /^[A-Za-z]$/.test(ch);
+}
+function isOperator(ch) {
+    return ["+", "-", "*", "/"].includes(ch);
+}
+
+
 {
    const buttons =document.querySelectorAll("button");
    const msg = document.querySelector(".display");
-
+   const oper= document.querySelector(".operator")
    buttons.forEach(btn => {
     btn.addEventListener("click",function(){
         let value =btn.innerText;
@@ -18,15 +26,23 @@ window.onload=() => {
             catch{
                 msg.value="error";
             }
-            else if(value==="DEL")
-           {
-               msg.value=msg.value.slice(0,-1);
-            }
+            
+            else if(value==="DEL"){
+                const lastChar = msg.value.slice(-1);  // get last character
+
+                if (isAlpha(lastChar)) {
+                    msg.value = "";                  // alph → reset
+                } else {
+                    msg.value = msg.value.slice(0, -1);  // number/symbol → delete 1 char
+                }}
+                 if (isOperator(lastChar) && isOperator(value)) {
+                     msg.value = msg.value.slice(0, -1) + value;   // replace operator
+                     }
              else {
                 msg.value+=value;
             }
-
-    })
+            
+        })
     
    });
 }
