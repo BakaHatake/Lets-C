5ac7a99a744c35de89d27f886e8158786700a5a1
akashhogetapal <akash85486@gmail.com>
2025-11-21 22:50:07 +0530

    cal

--- js/calc.js
+++ js/calc.js
@@ -1,48 +1,53 @@
-window.onload=() => {
-    
-}
+window.onload = () => {};
+
 function isAlpha(ch) {
     return /^[A-Za-z]$/.test(ch);
 }
+
 function isOperator(ch) {
     return ["+", "-", "*", "/"].includes(ch);
 }
 
-
 {
-   const buttons =document.querySelectorAll("button");
-   const msg = document.querySelector(".display");
-   const oper= document.querySelector(".operator")
-   buttons.forEach(btn => {
-    btn.addEventListener("click",function(){
-        let value =btn.innerText;
-        if(value==="C")
-            {
-            msg.value="";
-            } else if(value==="=")
-            try {
-                msg.value=eval(msg.value);
-            }
-            catch{
-                msg.value="error";
-            }
-            
-            else if(value==="DEL"){
-                const lastChar = msg.value.slice(-1);  // get last character
+    const buttons = document.querySelectorAll("button");
+    const msg = document.querySelector(".display");
 
-                if (isAlpha(lastChar)) {
-                    msg.value = "";                  // alph → reset
-                } else {
-                    msg.value = msg.value.slice(0, -1);  // number/symbol → delete 1 char
-                }}
-                 if (isOperator(lastChar) && isOperator(value)) {
-                     msg.value = msg.value.slice(0, -1) + value;   // replace operator
-                     }
-             else {
-                msg.value+=value;
-            }
+    buttons.forEach(btn => {
+        btn.addEventListener("click", function () {
+            let value = btn.innerText;
+            let lastChar = msg.value.slice(-1);
+
             
-        })
-    
-   });
+            if (value === "C") {
+                msg.value = "";
+            }
+
+            
+            else if (value === "=") {
+                try {
+                    msg.value = eval(msg.value);
+                } catch {
+                    msg.value = "error";
+                }
+            }
+
+            
+            else if (value === "DEL") {
+                if (isAlpha(lastChar)) {
+                    msg.value = "";     
+                } else {
+                    msg.value = msg.value.slice(0, -1); 
+                }
+            }
+
+            else if (isOperator(lastChar)) {
+                msg.value = msg.value.slice(0, -1) + value;
+            }
+
+            else {
+                msg.value += value;
+            }
+
+        });
+    });
 }
